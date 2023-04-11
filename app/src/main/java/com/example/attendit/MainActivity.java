c6cb0009751cb9f9656226151c407d75138c0d2d
Charan Singh <charansingh25082002@gmial.com>
2023-04-12 01:57:04 +0530

    second commit - vid 7 (14:00)

--- app/src/main/java/com/example/attendit/MainActivity.java
+++ app/src/main/java/com/example/attendit/MainActivity.java
@@ -2,13 +2,34 @@ package com.example.attendit;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+//import androidx.appcompat.widget.Toolbar;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
     FloatingActionButton fab;
+    RecyclerView recyclerView;
+    ClassAdapter classAdapter;
+    RecyclerView.LayoutManager layoutManager;
+    ArrayList<ClassItem> classItems = new ArrayList<>();
+    Toolbar toolbar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,9 +38,52 @@ public class MainActivity extends AppCompatActivity {
 
         fab = findViewById(R.id.fab_main);
         fab.setOnClickListener(v->showDialog());
+
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        classAdapter = new ClassAdapter(this, classItems);
+        recyclerView.setAdapter(classAdapter);
+        classAdapter.setOnItemClickListener(position -> gotoItemActivity(position));
+        setToolbar();
+
+
+    }
+
+    private void setToolbar() {
+        toolbar = findViewById(R.id.toolbar);
+        TextView title = toolbar.findViewById(R.id.title_toolbar);
+        TextView subtitle = toolbar.findViewById(R.id.subtitle_toolbar);
+        ImageButton back = toolbar.findViewById(R.id.back);
+        ImageButton save = toolbar.findViewById(R.id.save);
+
+        title.setText("Attend IT");
+        subtitle.setVisibility(View.GONE);
+        back.setVisibility(View.INVISIBLE);
+        save.setVisibility(View.INVISIBLE);
+    }
+
+    private void gotoItemActivity(int position) {
+        Intent intent = new Intent(this, StudentActivity.class);
+
+        intent.putExtra("className",classItems.get(position).getClassName());
+        intent.putExtra("subjectName",classItems.get(position).getSubjectName());
+        intent.putExtra("position", position);
+        startActivity(intent);
     }
 
     private void showDialog(){
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        MyDialog dialog = new MyDialog();
+        dialog.show(getSupportFragmentManager(), MyDialog.CLASS_ADD_DIALOG);
+        dialog.setListener((className,subjectName)->addClass(className,subjectName));
+
+    }
+
+    private void addClass(String className, String subjectName) {
+
+        classItems.add(new ClassItem(className, subjectName));
+        classAdapter.notifyDataSetChanged();
     }
 }
